b6411ca60bb7d3ab8b822be4264d8d2afd656c75
QUDUSKUNLE <quduskunle@gmail.com>
2021-04-13 22:47:41 +0100

    Refactor HapiJs routes

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const dotenv = require('dotenv');
 const basicAuthBasic = require('hapi-auth-basic');
 
 const auth = require('./src/middleware/basicAuthentication');
-const HapiController = require('./src/controllers/hapiController');
-const paths = require('./src/helpers/path');
+const routes = require('./src/routes');
 
 dotenv.config();
 const { validate } = auth;
@@ -15,51 +14,14 @@ const app = async () => {
   await server.register(basicAuthBasic);
   server.auth.strategy('simple', 'basic', { validate });
 
-  server.route([
-    {
-      method: paths.methods.get,
-      path: paths.route.home,
-      handler: async (req, res) => {
-        const response = await HapiController.home(req);
-        return res.response(response).code(200);
-      },
-    },
-    {
-      method: paths.methods.post,
-      path: paths.route.signup,
-      handler: async (req, res) => {
-        const response = await HapiController.signup(req);
-        if (!response.success) return res.response(response).code(409);
-        return res.response(response).code(201);
-      },
-    },
-    {
-      method: paths.methods.post,
-      path: paths.route.signin,
-      options: {
-        log: {
-          collect: true,
-        },
-      },
-      handler: async (req, res) => {
-        req.log('error', 'Event error');
-        const response = await HapiController.signin(req);
-        if (!response.success) return res.response(response).code(400);
-        return res.response(response).code(200);
-      },
-    },
-  ]);
-
+  server.route(routes);
   await server.start();
-  console.log(
-    `Server running on ${server.info.uri}`,
-  );
+  console.log(`Server running on ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
-
 
 app();
